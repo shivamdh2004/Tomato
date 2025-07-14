83ef69446cc8cc004036c7a2906311d349ed445c
Shivam Singh Dhakar <shivamsinghdhakar@gmail.com>
2025-07-15 00:40:19 +0530

    feat: Update database connection to use environment variable for MongoDB URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
     await mongoose.connect(
-      "mongodb+srv://shivamsinghdhakar:ktd1UvI1WObXQShR@cluster0.o6wokgq.mongodb.net/toma-togo"
+      `${process.env.MONGODB_URI}`
     ).then(()=>console.log("db connected"))
 }
