dfb5a80a91be19de6035d3a83ac1044e3380d8b9
Shivam Singh Dhakar <shivamsinghdhakar@gmail.com>
2025-07-15 01:37:56 +0530

    feat: Implement user authentication with login and registration endpoints, and integrate with frontend components

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,6 +6,9 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props)=>{
 
     const [cartItems,setCartItems] = useState({})
+    const url = "http://localhost:4000"
+    const [token,setToken] = useState("") 
+
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
           setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
@@ -31,12 +34,22 @@ const StoreContextProvider = (props)=>{
         }
         return totalAmount;
     }
+
+    useEffect(()=>{
+        if(localStorage.getItem("token")){
+            setToken(localStorage.getItem("token"));
+        }
+    })
+
 const contextValue = {
     food_list,
     cartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
 }
 
 return(
--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,11 +1,51 @@
 import React, { useState } from 'react'
 import "./LoginPopup.css"
 import { assets } from '../../assets/assets'
+import { useEffect } from 'react'
+import { useContext } from 'react'
+import { StoreContext } from '../../Context/StoreContext'
+import axios from "axios"
+import {toast} from "react-toastify"
 const LoginPopup = ({setShowLogin}) => {
-    const [currState,setCurrState] = useState("Sign Up")
+
+  const { setToken, url, loadCartData } = useContext(StoreContext);
+  const [currState, setCurrState] = useState("Sign Up");
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+
+    let new_url = url;
+    if (currState === "Login") {
+      new_url += "/api/user/login";
+    } else {
+      new_url += "/api/user/register";
+    }
+    const response = await axios.post(new_url, data);
+    if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      setShowLogin(false);
+    } else {
+      toast.error(response.data.message);
+    }
+  };
+
+    
   return (
     <div className="login-popup">
-      <form action="" className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className="login-popup-title">
           <h2>{currState}</h2>
           <img
@@ -18,12 +58,12 @@ const LoginPopup = ({setShowLogin}) => {
           {currState === "Login" ? (
             <></>
           ) : (
-            <input type="text" placeholder="Your Name" required />
+            <input type="text" name='name' onChange={onChangeHandler} value={data.name} placeholder="Your Name" required />
           )}
-          <input type="email" placeholder="Your Email" required />
-          <input type="password" placeholder="Password" required />
+          <input type="email" name='email' onChange={onChangeHandler} value={data.email} placeholder="Your Email" required />
+          <input type="password" name='password' onChange={onChangeHandler} value={data.password} placeholder="Password" required />
         </div>
-        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <button type='submit'>{currState === "Sign Up" ? "Create account" : "Login"}</button>
         <div className="login-popup-condition">
           <input type="checkbox" required />
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
